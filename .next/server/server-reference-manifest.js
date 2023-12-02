e6761ff16f65fdd01137b2079798671db98c829b
Austin Lord <alord@driven.care>
2023-12-02 11:27:17 -0500

    solved day 1 part 2

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Ga+H9/KehsItJG8LoTQyGTFy9yKltT3uerM/3lVutUg=\"\n}"
--- src/app/001/page.tsx
+++ src/app/001/page.tsx
@@ -17,6 +17,52 @@ export default function One() {
         return sum;
     }, [input]);
 
+    const numberTokens: { [key: string]: number } = {
+        "zero": 0,
+        "one": 1,
+        "two": 2,
+        "three": 3,
+        "four": 4,
+        "five": 5,
+        "six": 6,
+        "seven" : 7,
+        "eight": 8,
+        "nine": 9,
+        "1": 1,
+        "2": 2,
+        "3": 3,
+        "4": 4,
+        "5": 5,
+        "6": 6,
+        "7": 7,
+        "8": 8,
+        "9": 9,
+    }
+
+    const answer2 = useMemo(() => {
+        const lines = input.split("\n") ?? [];
+        const sum = lines.reduce((acc, line) => {
+            const occurrences = Object.keys(numberTokens).flatMap((key) => {
+                let indices: {key: string, index: number}[] = [];
+                let startIndex = 0;
+                let index;
+                while ((index = line.indexOf(key, startIndex)) > -1) {
+                    indices.push({ key, index });
+                    startIndex = index + 1;
+                }
+                return indices;
+            }); 
+            
+            const firstNumber = occurrences.reduce((a, b) => a.index < b.index ? a : b, { key: "error", index: 9999 });
+            const lastNumber = occurrences.reduce((a, b) => a.index > b.index ? a : b, { key: "error", index: 0 });
+            const calibrationNumber = parseInt(`${numberTokens[firstNumber.key]}${numberTokens[lastNumber.key]}`);
+
+            return acc + calibrationNumber;
+        }, 0);
+
+        return sum;
+    }, [input]);
+
     return (
         <div className="h-full ">
             <div className="w-4/5 mx-auto">  
@@ -28,7 +74,8 @@ export default function One() {
                         value={input} onChange={(e) => setInput(e.target.value)}
                     ></textarea>
                     <div className="w-1/2">
-                        <p>{answer1}</p>
+                        <p>The sum of calibration numbers is {answer1}</p>
+                        <p>The sum of calibration numbers, including spelled out numbers, is {answer2}</p>
                     </div>
                 </div>
             </div>
